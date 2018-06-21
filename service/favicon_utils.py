da84d6f864c89dff1c5ee5cc9f562969637ac568
tom mccormack <tmccormack2008@gmail.com>
2018-06-21 16:59:09 -0400

    Modified return values to include http status and error message

--- service/favicon_utils.py
+++ service/favicon_utils.py
@@ -8,35 +8,33 @@ FAVICON_TEXT = 'favicon.ico'
 def getFaviconUrl(target_url):
 	
     if not target_url:
-        print(f'Missing target url')
-        return None
+        msg = f'Missing target url'
+        return 400, None, msg
 
     temp_url = parse.urlsplit(target_url, scheme='https')
     print(temp_url)
 
     # if we don't have either a netloc or a path component, bail
     if not temp_url.netloc and not temp_url.path:
-        print(f'Invalid target url {target_url}')
-        return None
+        msg = f'Invalid target url {target_url}'
+        return 400, None, msg
 
     base_url = ''
     if temp_url.netloc:
     	base_url = '{0}://{1}'.format(temp_url.scheme, temp_url.netloc)
     else:
     	base_url = '{0}://{1}'.format(temp_url.scheme, temp_url.path)
-    print(base_url)
 
     favicon_url = parse.urljoin(base_url, FAVICON_TEXT)
 
     try:
     	resp = requests.get(favicon_url)
     except ConnectionError:
-    	print(f'Unable to reach website {base_url}')
-    	return None
-    
-    print(resp)
+    	msg = f'Unable to reach website {base_url}'
+    	return 404, None, msg
 
     if resp.status_code == 200:
-        return favicon_url
+        return 200, favicon_url, ''
     else:
-        print(f'Favicon not found at {favicon_url}')
+        msg = f'Favicon not found at {favicon_url}'
+        return resp.status_code, None, msg
--- service/handler.py
+++ service/handler.py
@@ -7,42 +7,37 @@ def handle_geturl(event, context):
 
     target = event['queryStringParameters']['target']
 
-    fav_url = getFaviconUrl(target)
+    status, ico_url, msg = getFaviconUrl(target)
 
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    return_msg = ico_url if status == 200 else msg
 
-    print(event['queryStringParameters'])
-    print(target)
 
     response = {
-        "statusCode": 200,
-        "body": json.dumps(fav_url)
+        "statusCode": status,
+        "body": json.dumps(return_msg)
     }
 
     return response
 
 
 if __name__ == "__main__":
-    ico_url = getFaviconUrl('https://www.google.com')
-    print(ico_url)
+    status, ico_url, msg = getFaviconUrl('https://www.google.com')
+    print(status, ico_url, msg)
 
-    ico_url = getFaviconUrl('https://www.google.com/tom')
-    print(ico_url)
+    status, ico_url, msg  = getFaviconUrl('https://www.google.com/tom')
+    print(status, ico_url, msg)
  
-    ico_url = getFaviconUrl('https://www.google.com:443/dick')
-    print(ico_url)
+    status, ico_url, msg  = getFaviconUrl('https://www.google.com:443/dick')
+    print(status, ico_url, msg)
    
-    ico_url = getFaviconUrl('')
-    print(ico_url)
+    status, ico_url, msg  = getFaviconUrl('')
+    print(status, ico_url, msg)
   
-    ico_url = getFaviconUrl(None)
-    print(ico_url)
+    status, ico_url, msg  = getFaviconUrl(None)
+    print(status, ico_url, msg)
   
-    ico_url = getFaviconUrl('www.google.com')
-    print(ico_url)
+    status, ico_url, msg  = getFaviconUrl('www.google.com')
+    print(status, ico_url, msg)
   
-    ico_url = getFaviconUrl('https://www.dsfgsdf.com')
-    print(ico_url)
+    status, ico_url, msg  = getFaviconUrl('https://www.dsfgsdf.com')
+    print(status, ico_url, msg)
