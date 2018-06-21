e11e8511e778264a4b8f17fa02e5d26835cdd725
tom mccormack <tmccormack2008@gmail.com>
2018-06-21 16:20:43 -0400

    Modified entry point to use 'target' query parameter

--- service/favicon_utils.py
+++ service/favicon_utils.py
@@ -5,23 +5,33 @@ from urllib import parse
 
 FAVICON_TEXT = 'favicon.ico'
 
-def getFaviconUrl(baseurl):
+def getFaviconUrl(target_url):
 	
-    if not baseurl:
-        print(f'Missing baseurl')
+    if not target_url:
+        print(f'Missing target url')
         return None
 
-    test_url = parse.urlsplit(baseurl)
-    if not all([test_url.scheme, test_url.netloc]):
-        print(f'Invalid baseurl {baseurl}')
+    temp_url = parse.urlsplit(target_url, scheme='https')
+    print(temp_url)
+
+    # if we don't have either a netloc or a path component, bail
+    if not temp_url.netloc and not temp_url.path:
+        print(f'Invalid target url {target_url}')
         return None
 
-    favicon_url = parse.urljoin(baseurl, FAVICON_TEXT)
+    base_url = ''
+    if temp_url.netloc:
+    	base_url = '{0}://{1}'.format(temp_url.scheme, temp_url.netloc)
+    else:
+    	base_url = '{0}://{1}'.format(temp_url.scheme, temp_url.path)
+    print(base_url)
+
+    favicon_url = parse.urljoin(base_url, FAVICON_TEXT)
 
     try:
     	resp = requests.get(favicon_url)
     except ConnectionError:
-    	print(f'Unable to reach website {favicon_url}')
+    	print(f'Unable to reach website {base_url}')
     	return None
     
     print(resp)
--- service/handler.py
+++ service/handler.py
@@ -5,12 +5,17 @@ from favicon_utils import getFaviconUrl
 
 def handle_geturl(event, context):
 
-    fav_url = getFaviconUrl('https://www.google.com')
+    target = event['queryStringParameters']['target']
+
+    fav_url = getFaviconUrl(target)
 
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
+
+    print(event['queryStringParameters'])
+    print(target)
 
     response = {
         "statusCode": 200,
@@ -20,18 +25,24 @@ def handle_geturl(event, context):
     return response
 
 
-# if __name__ == "__main__":
-#     ico_url = getFaviconUrl('https://www.google.com')
-#     print(ico_url)
+if __name__ == "__main__":
+    ico_url = getFaviconUrl('https://www.google.com')
+    print(ico_url)
+
+    ico_url = getFaviconUrl('https://www.google.com/tom')
+    print(ico_url)
+ 
+    ico_url = getFaviconUrl('https://www.google.com:443/dick')
+    print(ico_url)
+   
+    ico_url = getFaviconUrl('')
+    print(ico_url)
   
-#     ico_url = getFaviconUrl('')
-#     print(ico_url)
+    ico_url = getFaviconUrl(None)
+    print(ico_url)
   
-#     ico_url = getFaviconUrl(None)
-#     print(ico_url)
+    ico_url = getFaviconUrl('www.google.com')
+    print(ico_url)
   
-#     ico_url = getFaviconUrl('www.google.com')
-#     print(ico_url)
-  
-#     ico_url = getFaviconUrl('https://www.dsfgsdf.com')
-#     print(ico_url)
+    ico_url = getFaviconUrl('https://www.dsfgsdf.com')
+    print(ico_url)
